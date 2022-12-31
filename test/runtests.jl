f373546b213e261c15e423c5ba4ba156eab7a2fc
Carlos A. Michelén Ströfer <c.michelen.strofer@gmail.com>
2022-12-30 20:48:31 -0800

    doctests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test, SafeTestsets, Documenter
 
-@time @testset "DimensionfulAngles.jl" begin
+@time @testset verbose=true "DimensionfulAngles.jl" begin
     @time @safetestset "Test Core Package" begin include("test_core_package.jl") end
     @time @safetestset "Test Base Functions" begin include("test_base_functions.jl") end
-    # TODO: documenter
+    @time @safetestset "Doc Tests" begin include("test_doctest.jl") end
 end
---
+++ test/test_doctest.jl
@@ -0,0 +1,3 @@
+using Test, Documenter, DimensionfulAngles
+
+doctest(DimensionfulAngles)
